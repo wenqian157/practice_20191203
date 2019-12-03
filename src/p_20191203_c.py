1cad1524f3222609cc68a3b8686780ae74487627
wenqian157 <44199787+wenqian157@users.noreply.github.com>
2019-12-03 14:53:28 +0100

    11

--- src/p_20191203_c.py
+++ src/p_20191203_c.py
@@ -3,7 +3,11 @@ from p_20191203_b import *
 
 branch = 'new'
 branch = 'master again'
+<<<<<<< Updated upstream
 change = 2
+=======
+change = 10
+>>>>>>> Stashed changes
 
 def print_all():
     func()
